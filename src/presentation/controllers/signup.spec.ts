6f3c7a5b396ba4d491fba4ea7e2a411ba5d14c04
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2020-09-25 08:57:35 -0300

    test: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -16,18 +16,18 @@ describe('SignUpController', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('name'))
     })
-    test('Should return 400 if no email is provided', () => {
+    test('Should return 400 if no password is provided', () => {
         const sut = new SignUpController()
         const httpRequest = {
             body: {
                 name: 'any',
-                // email: 'any_email@gmail.com',
-                password: 'any_password',
+                email: 'any_email@gmail.com',
+                // password: 'any_password',
                 passwordConfirmation: 'any_password'
             }
         }
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
-        expect(httpResponse.body).toEqual(new MissingParamError('email'))
+        expect(httpResponse.body).toEqual(new MissingParamError('password'))
     })
 })
