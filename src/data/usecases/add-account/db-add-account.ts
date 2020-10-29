d81dd08e27ce88a1b30a5433d2dc8ae671b9c9af
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2020-10-29 15:59:12 -0300

    test: ensure DbAddAccount throws if Encrypter throws

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -3,7 +3,7 @@ import { AddAccount, AddAccountModel } from './../../../domain/usecases/add-acco
 import { Encrypter } from '../../protocols/encrypter'
 
 export class DbAddAccount implements AddAccount {
-    private encrypter: Encrypter
+    private readonly encrypter: Encrypter
 
     constructor (encrypter: Encrypter) {
         this.encrypter = encrypter
