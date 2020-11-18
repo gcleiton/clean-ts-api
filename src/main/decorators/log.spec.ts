bdf055d61df1d95a3c58bd9f8cc5721fcc2e252b
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2020-11-18 20:01:17 -0300

    test: ensure LogMongoRepository creates an error log on success

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,8 +1,8 @@
-import { LogErrorRepository } from "../../data/protocols/log-error-repository"
-import { AccountModel } from "../../domain/models/account"
-import { serverError, ok } from "../../presentation/helpers/http-helper"
-import { Controller, HttpRequest, HttpResponse } from "../../presentation/protocols"
-import { LogControllerDecorator } from "./log"
+import { LogErrorRepository } from '../../data/protocols/log-error-repository'
+import { AccountModel } from '../../domain/models/account'
+import { serverError, ok } from '../../presentation/helpers/http-helper'
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+import { LogControllerDecorator } from './log'
 
 describe('LogController Decorator', () => {
     const makeController = (): Controller => {
@@ -16,7 +16,7 @@ describe('LogController Decorator', () => {
 
     const makeLogErrorRepository = (): LogErrorRepository => {
         class LogErrorRepositoryStub implements LogErrorRepository {
-            async log (stack: string): Promise<void> {
+            async logError (stack: string): Promise<void> {
                 return new Promise(resolve => resolve())
             }
         }
@@ -78,7 +78,7 @@ describe('LogController Decorator', () => {
 
     test('Should call LogErrorRepository with correct error if controller returns a server error', async () => {
         const { sut, controllerStub, logErrorRepositoryStub } = makeSut()
-        const logSpy = jest.spyOn(logErrorRepositoryStub, 'log')
+        const logSpy = jest.spyOn(logErrorRepositoryStub, 'logError')
         jest.spyOn(controllerStub, 'handle').mockReturnValueOnce(new Promise(resolve => resolve(makeFakeServerError())))
         await sut.handle(makeFakeRequest())
         expect(logSpy).toHaveBeenCalledWith('any_stack')
