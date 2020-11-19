c8de0ed678be9d203a1fc9089bd3ab5c55556155
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2020-11-19 10:49:56 -0300

    refactor: remove code duplication

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -3,6 +3,7 @@ import { EmailValidator } from './../../protocols/email-validator';
 import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
 import { LoginController } from './login'
+import { HttpRequest } from '../../protocols';
 
 const makeEmailValidator = (): EmailValidator => {
     class EmailValidatorStub implements EmailValidator {
@@ -13,6 +14,13 @@ const makeEmailValidator = (): EmailValidator => {
 
     return new EmailValidatorStub()
 }
+
+const makeFakeRequest = (): HttpRequest => ({
+    body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+    }
+})
 
 interface SutTypes {
     sut: LoginController
@@ -54,26 +62,14 @@ describe('Login Controller', () => {
     test('Should return 400 if an invalid email is provided', async () => {
         const { sut, emailValidatorStub } = makeSut()
         jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
-        const httpRequest = {
-            body: {
-                email: 'any_email@mail.com',
-                password: 'any_password'
-            }
-        }
-        const httpResponse = await sut.handle(httpRequest)
+        const httpResponse = await sut.handle(makeFakeRequest())
         expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
     })
 
     test('Should call EmailValidator with correct email', async () => {
         const { sut, emailValidatorStub } = makeSut()
         const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
-        const httpRequest = {
-            body: {
-                email: 'any_email@mail.com',
-                password: 'any_password'
-            }
-        }
-        await sut.handle(httpRequest)
+        await sut.handle(makeFakeRequest())
         expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
     })
 })
