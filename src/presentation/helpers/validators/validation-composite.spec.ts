aaaa7fdc31defeecf99e8827691c552bdc0fe9ea
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2020-11-20 09:26:40 -0300

    refactor: replace test names

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -29,14 +29,14 @@ const makeSut = (): SutTypes => {
 }
 
 describe('Validation Composite', () => {
-    test('Should return the first error if more then one validation fails', () => {
+    test('Should return an error if any validation fails', () => {
         const { sut, validationStubs } = makeSut()
         jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
         const error = sut.validate({ field: 'any_value' })
         expect(error).toEqual(new MissingParamError('field'))
     })
 
-    test('Should return an error if any validation fails', () => {
+    test('Should return the first error if more then one validation fails', () => {
         const { sut, validationStubs } = makeSut()
         jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error())
         jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
