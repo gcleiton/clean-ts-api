5d465087229753141c872e10e9b6ff2eec340b7e
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2020-11-22 15:05:41 -0300

    refactor: move data layer interface to folders

--- src/data/protocols/criptography/encrypter.ts
+++ src/data/protocols/criptography/encrypter.ts
@@ -1,5 +1,3 @@
-import { AccountModel } from "../../domain/models/account";
-
 export interface Encrypter {
     encrypt (value: string): Promise<string>
 }
--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,5 +1,6 @@
 import { AccountModel } from "../../../domain/models/account"
-import { LoadAccountByEmailRepository } from './../../protocols/load-account-by-email-repository'
+import { AuthenticationModel } from "../../../domain/usecases/authentication"
+import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 import { DbAuthentication } from "./db-authentication"
 
 const makeFakeAccount = (): AccountModel => ({
