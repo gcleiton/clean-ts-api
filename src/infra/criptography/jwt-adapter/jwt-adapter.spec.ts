bb703ce537e4432b92283ca948a1f1e9edd1e723
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2020-11-23 12:03:11 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,3 +1,4 @@
+import { rejects } from 'assert'
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
@@ -21,5 +22,14 @@ describe('Jwt Adapter', () => {
         const sut = new JwtAdapter('secret')
         const accessToken = await sut.encrypt('any_id')
         expect(accessToken).toBe('any_token')
+    })
+
+    test('Should throw if sign throws', async () => {
+        const sut = new JwtAdapter('secret')
+        jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+            throw new Error()
+        })
+        const promise = sut.encrypt('any_id')
+        await expect(promise).rejects.toThrow()
     })
 })
