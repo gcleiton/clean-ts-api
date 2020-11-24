60578b1941c33ed51341348499bb191058b8c7ce
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2020-11-24 11:27:47 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/auth-routes.test.ts
+++ src/main/routes/auth-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
     beforeAll(async () => {
         await MongoHelper.connect(process.env.MONGO_URL)
     })
@@ -16,15 +16,17 @@ describe('SignUp Routes', () => {
         await MongoHelper.disconnect()
     })
 
-    test('Should return an account on success', async () => {
-        await request(app)
-            .post('/api/signup')
-            .send({
-                name: 'Gabriel Cleiton',
-                email: 'ccgabrielcc2609@gmail.com',
-                password: '12345',
-                passwordConfirmation: '12345'
-            })
-            .expect(200)
+    describe('POST /signup', () => { 
+        test('Should return 200 on success', async () => {
+            await request(app)
+                .post('/api/signup')
+                .send({
+                    name: 'Gabriel Cleiton',
+                    email: 'ccgabrielcc2609@gmail.com',
+                    password: '12345',
+                    passwordConfirmation: '12345'
+                })
+                .expect(200)
+        })
     })
 })
