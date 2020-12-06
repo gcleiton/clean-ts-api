e4409db5faf0a3e931e9854252ab772f2e47fb52
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2020-12-06 16:21:14 -0300

    feat: ensure DbLoadAccountByToken calls LoadAccountByTokenRepository with correct values

---
+++ src/data/protocols/db/account/load-account-by-token-repository.ts
@@ -0,0 +1,6 @@
+import { AccountModel } from '../../../../domain/models/account'
+
+export interface LoadAccountByTokenRepository {
+    loadByToken (token: string, role?: string): Promise<AccountModel>
+
+}
