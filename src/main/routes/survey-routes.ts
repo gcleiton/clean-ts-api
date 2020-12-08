2bd7fd3c6dd6ad85a7613d28344a8240cb94cfa1
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2020-12-07 22:00:03 -0300

    fix: ensure AccountMongoRepository allows admin to access any route

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -5,6 +5,6 @@ import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 
 export default (router: Router): void => {
-    const adminAuth = adaptMiddleware(makeAuthMiddleware())
+    const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
     router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
 }
