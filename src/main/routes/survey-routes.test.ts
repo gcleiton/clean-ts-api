300a5441eb39e6b54ae2698a3dd5779e80537862
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2020-12-07 21:21:12 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Routes', () => {
     beforeAll(async () => {
@@ -11,8 +14,10 @@ describe('Survey Routes', () => {
     })
 
     beforeEach(async () => {
-        surveyCollection = await MongoHelper.getCollection('accounts')
+        surveyCollection = await MongoHelper.getCollection('surveys')
         await surveyCollection.deleteMany({})
+        accountCollection = await MongoHelper.getCollection('accounts')
+        await accountCollection.deleteMany({})
     })
 
     afterAll(async () => {
@@ -33,6 +38,38 @@ describe('Survey Routes', () => {
                     }]
                 })
                 .expect(403)
+        })
+
+        test('Should return 204 with valid accessToken', async () => {
+            const res = await accountCollection.insertOne({
+                name: 'Gabriel Cleiton',
+                email: 'ccgabrielcc2609@gmail.com',
+                password: '123',
+                role: 'admin'
+            })
+            const id = res.ops[0]._id
+            const accessToken = sign({ id }, env.jwtSecret)
+            await accountCollection.updateOne({
+                _id: id
+            }, {
+                $set: {
+                    accessToken
+                }
+            })
+
+            await request(app)
+                .post('/api/surveys')
+                .set('x-access-token', accessToken)
+                .send({
+                    question: 'Question',
+                    answers: [{
+                        answer: 'Answer 1',
+                        image: 'http://image-name.com'
+                    }, {
+                        answer: 'Answer 2'
+                    }]
+                })
+                .expect(204)
         })
     })
 })
