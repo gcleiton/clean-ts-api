e1db01019fdb55ab058cca6a930048877068dbfb
Gabriel Cleiton <ccgabrielcc2609@gmail.com>
2020-12-08 21:06:23 -0300

    test: ensure DbLoadSurveys returns a list of surveys on success

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -1,57 +1,72 @@
+import MockDate from 'mockdate'
 import { SurveyModel } from '../../../domain/models/survey'
 import { LoadSurveysRepository } from './../../protocols/db/survey/load-surveys-repository'
 import { DbLoadSurveys } from './db-load-surveys'
 
+const makeFakeSurveys = (): SurveyModel[] => {
+    return [{
+        id: 'any_id',
+        question: 'any_question',
+        answers: [{
+            image: 'any_image',
+            answer: 'any_answer'
+        }],
+        date: new Date()
+    }, {
+        id: 'other_id',
+        question: 'other_question',
+        answers: [{
+            image: 'other_image',
+            answer: 'other_answer'
+        }],
+        date: new Date()
+    }]
+}
+
+const makeLoadSurveysRepository = (): LoadSurveysRepository => {
+    class LoadSurveysRepositoryStub implements LoadSurveysRepository {
+        async loadAll (): Promise<SurveyModel[]> {
+            return new Promise(resolve => resolve(makeFakeSurveys()))
+        }
+    }
+
+    return new LoadSurveysRepositoryStub()
+}
+
+interface SutTypes {
+    sut: DbLoadSurveys
+    loadSurveysRepositoryStub: LoadSurveysRepository
+}
+
+const makeSut = (): SutTypes => {
+    const loadSurveysRepositoryStub = makeLoadSurveysRepository()
+    const sut = new DbLoadSurveys(loadSurveysRepositoryStub)
+
+    return {
+        sut,
+        loadSurveysRepositoryStub
+    }
+}
+
 describe('DbLoadSurveys', () => {
-    const makeFakeSurveys = (): SurveyModel[] => {
-        return [{
-            id: 'any_id',
-            question: 'any_question',
-            answers: [{
-                image: 'any_image',
-                answer: 'any_answer'
-            }],
-            date: new Date()
-        }, {
-            id: 'other_id',
-            question: 'other_question',
-            answers: [{
-                image: 'other_image',
-                answer: 'other_answer'
-            }],
-            date: new Date()
-        }]
-    }
+    beforeAll(() => {
+        MockDate.set(new Date())
+    })
 
-    const makeLoadSurveysRepository = (): LoadSurveysRepository => {
-        class LoadSurveysRepositoryStub implements LoadSurveysRepository {
-            async loadAll (): Promise<SurveyModel[]> {
-                return new Promise(resolve => resolve(makeFakeSurveys()))
-            }
-        }
-
-        return new LoadSurveysRepositoryStub()
-    }
-
-    interface SutTypes {
-        sut: DbLoadSurveys
-        loadSurveysRepositoryStub: LoadSurveysRepository
-    }
-
-    const makeSut = (): SutTypes => {
-        const loadSurveysRepositoryStub = makeLoadSurveysRepository()
-        const sut = new DbLoadSurveys(loadSurveysRepositoryStub)
-
-        return {
-            sut,
-            loadSurveysRepositoryStub
-        }
-    }
+    afterAll(() => {
+        MockDate.reset()
+    })
 
     test('Should call LoadSurveysRepository', async () => {
         const { sut, loadSurveysRepositoryStub } = makeSut()
         const loadAllSpy = jest.spyOn(loadSurveysRepositoryStub, 'loadAll')
         await sut.load()
         expect(loadAllSpy).toHaveBeenCalled()
+    })
+
+    test('Should return a list of Surveys on success', async () => {
+        const { sut } = makeSut()
+        const surveys = await sut.load()
+        expect(surveys).toEqual(makeFakeSurveys())
     })
 })
